6774e72dba1f92756db0ba277e513c860a144294
Kamil Mikuła <kamimik090@student.polsl.pl>
2024-01-06 23:22:34 +0100

    Initial

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+let item = "<h1>KURWA CD</h1> renderujemy całe html i je wstawiamy pod routy";
+app.get("/", (req, res) => {
+  res.send("Hello, Express!");
+});
+app.get("/about", (req, res) => {
+  res.send(item);
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
